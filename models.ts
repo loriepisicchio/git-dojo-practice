5cdbf5e50fdeeea0207c6c90e3c9e57364ad7f80
Gaël Jourdan-Weil <gjourdanweil@gmail.com>
2019-09-22 17:38:36 +0200

    Create models.ts

---
+++ models.ts
@@ -0,0 +1,4 @@
+
+// In branch 'fix/typo': rename the type to Student
+// In branch 'feature/formatting': reformat the type on multiple lines for better readibility
+type Teacher = { name: string; year: '1A' | '2A' | '3A'; email: string }
